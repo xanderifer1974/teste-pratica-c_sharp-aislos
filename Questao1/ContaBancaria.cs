3fbc9f8074232237738b8eb954f99d190d7c30d9
Alexandre Ferreira <xanderifer1974@gmail.com>
2025-02-03 18:57:12 -0300

    Criação da classe conta bancária

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -2,8 +2,44 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    class ContaBancaria
+    {
 
-       
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        private const double TaxaSaque = 3.50;
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Depositar(depositoInicial);
+        }
+
+
+        public void Depositar(double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Sacar(double quantia)
+        {
+            Saldo -= quantia + TaxaSaque;
+        }
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo:F2}";
+        }
+
+
+
+
+
     }
 }
