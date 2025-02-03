c4d2f7d142f4b898218563114b4fd2b3a69edcab
Alexandre Ferreira <xanderifer1974@gmail.com>
2025-02-03 18:30:00 -0300

    Ajuste na versão do exercicio para .net 8.0

--- Questao1/Program.cs
+++ Questao1/Program.cs
@@ -5,7 +5,7 @@ namespace Questao1 {
     class Program {
         static void Main(string[] args) {
 
-            ContaBancaria conta;
+            //ContaBancaria conta;
 
             Console.Write("Entre o número da conta: ");
             int numero = int.Parse(Console.ReadLine());
@@ -16,29 +16,29 @@ namespace Questao1 {
             if (resp == 's' || resp == 'S') {
                 Console.Write("Entre o valor de depósito inicial: ");
                 double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                conta = new ContaBancaria(numero, titular, depositoInicial);
+                //conta = new ContaBancaria(numero, titular, depositoInicial);
             }
             else {
-                conta = new ContaBancaria(numero, titular);
+                /*conta = new ContaBancaria(numero, titular);*/
             }
 
             Console.WriteLine();
             Console.WriteLine("Dados da conta:");
-            Console.WriteLine(conta);
+            //Console.WriteLine(conta);
 
             Console.WriteLine();
             Console.Write("Entre um valor para depósito: ");
             double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            conta.Deposito(quantia);
+            //conta.Deposito(quantia);
             Console.WriteLine("Dados da conta atualizados:");
-            Console.WriteLine(conta);
+            //Console.WriteLine(conta);
 
             Console.WriteLine();
             Console.Write("Entre um valor para saque: ");
             quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            conta.Saque(quantia);
+            //conta.Saque(quantia);
             Console.WriteLine("Dados da conta atualizados:");
-            Console.WriteLine(conta);
+            //Console.WriteLine(conta);
 
             /* Output expected:
             Exemplo 1:
