071852a09096e2c74229ee0f4a660d1a2c96b183
Alexandre Ferreira <xanderifer1974@gmail.com>
2025-02-06 18:56:05 -0300

    Finalizando a controller Movimentacao

--- Questao5/Infrastructure/Services/Controllers/MovimentacaoController.cs
+++ Questao5/Infrastructure/Services/Controllers/MovimentacaoController.cs
@@ -1,5 +1,8 @@
-using Microsoft.AspNetCore.Http;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Questao5.Businness.Interfaces;
+using Questao5.Domain.Request;
 
 namespace Questao5.Infrastructure.Services.Controllers
 {
@@ -7,6 +10,37 @@ namespace Questao5.Infrastructure.Services.Controllers
     [ApiController]
     public class MovimentacaoController : ControllerBase
     {
-        //Criar métodos e injeção de dependência
+        private readonly IMovimentacaoService _movimentacaoService;
+
+        public MovimentacaoController(IMovimentacaoService movimentacaoService)
+        {
+            _movimentacaoService = movimentacaoService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MovimentarContaCorrente([FromBody] MovimentacaoRequest request)
+        {
+            try
+            {
+                // Gerar a chave de idempotência
+                request.RequisicaoId = GerarChaveIdempotencia(request.ContaCorrenteId, request.Valor, request.TipoMovimento);
+
+                var movimento = await _movimentacaoService.MovimentarContaCorrenteAsync(request.RequisicaoId, request.ContaCorrenteId, request.Valor, request.TipoMovimento);
+                return Ok(movimento);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+        }
+
+
+        private string GerarChaveIdempotencia(string contaCorrenteId, decimal valor, char tipoMovimento)
+        {
+            var data = $"{contaCorrenteId}-{valor}-{tipoMovimento}-{DateTime.UtcNow.Ticks}";
+            using var sha256 = SHA256.Create();
+            var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(data));
+            return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+        }
     }
 }
