e2e402fff6315b3ecfd70ef34aa53048560228a7
Alexandre Ferreira <xanderifer1974@gmail.com>
2025-02-06 19:21:33 -0300

    Ajuste no método para salvar movimentação

--- Questao5/Domain/Request/MovimentacaoRequest.cs
+++ Questao5/Domain/Request/MovimentacaoRequest.cs
@@ -1,8 +1,11 @@
 
+using System.Text.Json.Serialization;
+
 namespace Questao5.Domain.Request;
 
 public class MovimentacaoRequest
 {
+    [JsonIgnore]
     public string? RequisicaoId { get; set; }
     public string? ContaCorrenteId { get; set; }
     public decimal Valor { get; set; }
