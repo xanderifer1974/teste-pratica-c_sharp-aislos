0d7c3efae0bdf93155df4af1a002e4be90897915
Alexandre Ferreira <xanderifer1974@gmail.com>
2025-02-07 07:27:16 -0300

    Limpeza das classes não utilizadas

--- Questao5/Application/ViewModel/ContaCorrenteViewModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Questao5.Application.ViewModel;
-
-public class ContaCorrenteViewModel
-{
-    //Colocar as propriedades e propriedades json
-}
--- Questao5/Application/ViewModel/MovimentacaoViewModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Questao5.Application.ViewModel;
-
-public class MovimentacaoViewModel
-{
-     //Colocar as propriedades e propriedades json
-}
